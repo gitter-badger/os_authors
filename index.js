910a6c77f667a4e5a1fa934f32b541464a93d1f5
reker- <reker.opensource@gmail.com>
2015-08-04 19:28:29 +0200

    Add 'load' & 'save' function

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+var jsonfile = require('jsonfile');
+
 module.exports = {
     init    : __init,
     add     : __add,
+    save    : __save,
+    load    : __load,
 };
 
 var g_Project = __init();
+
+function __init(opts) {
+    opts = opts || {};
+    return {
+        'meta'      : opts['meta'] || {},
+        'authors'   : opts['authors'] || [],
+    };
+}
 
 function __add(name, nick, mail, wbpg, opts) {
     opts = opts || {};
@@ -19,10 +31,16 @@ function __add(name, nick, mail, wbpg, opts) {
         g_Project['authors'].push(obj);
 }
 
-function __init(opts) {
+function __save(opts) {
     opts = opts || {};
-    return {
-        'meta'      : opts['meta']     || {},
-        'authors'   : opts['authors']  || [],
-    };
+    var obj = jsonfile.writeFileSync(opts['path'] || './.ath.json', opts['instance'] || g_Project);
+}
+
+function __load(opts) {
+    opts = opts || {};
+    var obj = jsonfile.readFileSync(opts['path'] || './.ath.json');
+    if (opts['instance'])
+        opts['instance'] = obj;
+    else
+        g_Project = obj;
 }
