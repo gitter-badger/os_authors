9917d885db8dcc5aeb16555d73ce7f1da8c3e115
reker- <reker.opensource@gmail.com>
2015-08-05 23:36:16 +0200

    Add CLI commands

--- bin/shell.js
+++ bin/shell.js
@@ -3,10 +3,29 @@
 var os_authors = require('../index.js');
 var yargs = require('yargs');
 
+function _getCommand() {
+    for (id in yargs.argv._) {
+        switch (yargs.argv._[id]) {
+            case 'init':
+                return 'init';
+            case 'add':
+                return 'add';
+            default:
+                return null;
+        }
+    }
+}
+
 var g_Authors = os_authors.init();
 var g_Path = yargs.argv['path'] || process.cwd();
+var g_Cmd = _getCommand();
+
+if (g_Cmd == null) {
+    process.exit(1); // print usage
+}
 
 os_authors.load({ 'path': (g_Path + '/.ath.json'), 'instance': g_Authors });
+
 
 
 os_authors.save({ 'path': (g_Path + '/.ath.json'), 'instance': g_Authors });
