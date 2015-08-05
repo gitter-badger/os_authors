6081eedc6a39c097f4328a3e88ea553a7d884cd4
reker- <reker.opensource@gmail.com>
2015-08-05 23:02:43 +0200

    Extend CLI

--- bin/shell.js
+++ bin/shell.js
@@ -1,3 +1,12 @@
 ﻿#! /usr/bin/env node
 
-      
+var os_authors = require('../index.js');
+var yargs = require('yargs');
+
+var g_Authors = os_authors.init();
+var g_Path = process.cwd();
+
+os_authors.load({ 'path': (g_Path + '/.ath.json'), 'instance': g_Authors });
+
+
+os_authors.save({ 'path': (g_Path + '/.ath.json'), 'instance': g_Authors });
