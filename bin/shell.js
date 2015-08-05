e4b17925a976482bcff66bd46d4744f6d911387f
reker- <reker.opensource@gmail.com>
2015-08-05 23:18:50 +0200

    Add --path option

--- bin/shell.js
+++ bin/shell.js
@@ -4,7 +4,7 @@ var os_authors = require('../index.js');
 var yargs = require('yargs');
 
 var g_Authors = os_authors.init();
-var g_Path = process.cwd();
+var g_Path = yargs.argv['path'] || process.cwd();
 
 os_authors.load({ 'path': (g_Path + '/.ath.json'), 'instance': g_Authors });
 
